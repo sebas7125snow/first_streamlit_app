8567396dda4df74bdd448b852003f62f0d30cb45
sebas7125snow <123031654+sebas7125snow@users.noreply.github.com>
2023-01-18 18:08:54 -0500

    Update streamlit_app.py
    Adding format to the subtitle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.title('Breakfast Menu', fontsize='small')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
